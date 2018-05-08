5187b504b52163757a8dd3a146b374a1d5a9d660
Andrew Burgess <andrew@andrewburgess.io>
2018-05-08 11:05:25 -0700

    :sparkles: feat(sequelize-error): make sequelize optional

--- src/sequelize-error.js
+++ src/sequelize-error.js
@@ -1,27 +1,25 @@
 import _ from 'lodash';
-import {
-    BadRequestError,
-    ConflictError
-} from '@revolttv/errors';
-import {
-    UniqueConstraintError,
-    ValidationError
-} from 'sequelize';
+import { BadRequestError, ConflictError } from '@revolttv/errors';
 
 export default () => {
     function sequelizeErrorHandler(err, req, res, next) {
-        if (err instanceof ValidationError) {
-            let message = `${err.message}: ${_.map(err.errors, 'message').join(', ')}`;
-            let fields = _.map(err.errors, 'path');
+        try {
+            const { UniqueConstraintError, ValidationError } = require('sequelize');
+            if (err instanceof ValidationError) {
+                let message = `${err.message}: ${_.map(err.errors, 'message').join(', ')}`;
+                let fields = _.map(err.errors, 'path');
 
-            if (err instanceof UniqueConstraintError) {
-                return next(new ConflictError(message, fields));
+                if (err instanceof UniqueConstraintError) {
+                    return next(new ConflictError(message, fields));
+                }
+
+                return next(new BadRequestError(message, fields));
             }
 
-            return next(new BadRequestError(message, fields));
+            return next(err);
+        } catch (requireError) {
+            return next(err);
         }
-
-        return next(err);
     }
 
     return sequelizeErrorHandler;
